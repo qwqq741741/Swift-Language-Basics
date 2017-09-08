f5a7ab52be9475010100c82cddd12145b63b3283
Wentao Song <wentaosong@Wentaos-MacBook-Pro.local>
2017-09-08 00:35:50 -0500

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Wentao Song on 2017/9/8.
+//  Copyright © 2017年 Wentao Song. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
